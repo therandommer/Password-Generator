ae8ef39967d9e0aaff0cae6a11e8018982aa055c
Isabella <bdc190799@gmail.com>
2023-08-29 14:43:01 +0100

    GetRandom() Complete
    GetRandom is now complete and will return a random index value from the provided array. GetPasswordOptions() now returns length as a number.

--- Scripts/script.js
+++ Scripts/script.js
@@ -98,11 +98,14 @@ function getPasswordOptions() {
     //Filters to int so we can collect the correct type of data for use later in the program. 
     length = parseInt(prompt("How many characters would you like your password to have? \n Enter a number between 10-64.", 10)); //specifies denary numbers, else other number types could be supported
   }
+  return length; //returns length as a number to be used in future logic.
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  let randomIndex = Math.floor(Math.random * arr.length); //random number to refer to specific character in array.
+  console.log(`Random value is ${arr[randomIndex]}`); //console log for debugging reference
+  return arr[randomIndex]; //returns the random index value selected earlier.
 }
 
 // Function to generate password with user input
@@ -117,7 +120,6 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-  getPasswordOptions();
   passwordText.value = password;
 }
 
